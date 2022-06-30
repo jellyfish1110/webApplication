80f6a7db2996ffde8e3c49dc2b29fd5c87b2a767
PC <p.simeonov@student.utwente.nl>
2022-06-30 13:00:34 +0300

    Played around with the css of homepage, added buttons instead of hyperlinks and fixed grid in the content of the homepage.

--- webApplication/src/main/java/com/example/webApplication/publicApplication/AppConfig.java
+++ webApplication/src/main/java/com/example/webApplication/publicApplication/AppConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Configuration
 public class AppConfig {
 
@@ -17,4 +20,17 @@ public class AppConfig {
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.$2A);
     }
+
+    /*
+        Not really sure if it's supposed to be declared as a bean,
+        but i put it here to use anywhere in the code in the case when
+        mapping a list is needed.
+     */
+//    @Bean
+//    public <S, T> List<T> mapList(List<S> source, Class<T> targetClass) {
+//        return source
+//                .stream()
+//                .map(element -> modelMapper().map(element, targetClass))
+//                .collect(Collectors.toList());
+//    }
 }
